7c02a29dd370200dbd9013825e8d7e5682d1db1e
Beelinx123 <test.beelinx@gmail.com>
2024-10-05 11:21:40 +0530

    Add files via upload

---
+++ src/test/java/com/example/Beelinx/BeelinxApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.Beelinx;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BeelinxApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
